315b03f52ce4b569bbe00baf5cd9bad1be2ce594
Tobias Zechmann <zechmann.tobias@t-online.de>
2022-11-03 18:17:20 +0100

    Initial commit

---
+++ src/main/java/de/sh00ckbass/minecraft/servermanager/ServerManager.java
@@ -0,0 +1,24 @@
+package de.sh00ckbass.minecraft.servermanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*******************************************************
+ * Copyright (C) Tobias Zechmann tobias@sh00ckbass.de
+ *
+ * This file is part of ServerManager and was created at the 03.11.2022
+ *
+ * ServerManager can not be copied and/or distributed without the express
+ * permission of the owner.
+ *
+ */
+public final class ServerManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
